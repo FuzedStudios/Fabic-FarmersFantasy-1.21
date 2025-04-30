9426cd03ab15d8757f9327529a10d951161e1d6a
LexPex636 <lexgroenewald2009@gmail.com>
2025-04-30 17:17:17 +0200

    Created Mod

---
+++ src/main/java/net/fuzed/farmersfantasy/FarmersFantasy.java
@@ -0,0 +1,18 @@
+package net.fuzed.farmersfantasy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FarmersFantasy implements ModInitializer {
+	public static final String MOD_ID = "farmersfantasy";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/fuzed/farmersfantasy/FarmersFantasyClient.java
@@ -0,0 +1,11 @@
+package net.fuzed.farmersfantasy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FarmersFantasyClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
